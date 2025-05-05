64cbaf8b8990f166d7ef5b2d22f00be5dd6c504a
eduard214 <octavianpantazi@yahoo.com>
2025-05-05 16:06:54 +0300

    Refactor project structure: update output type for Database project, rename Program.cs to DiConfig.cs, and streamline project references

---
+++ Database/DiConfig.cs
@@ -0,0 +1,16 @@
+﻿using Database.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Database;
+
+public static class DiConfig
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddDbContext<DatabaseContext>();
+        services.AddScoped<DbContext, DatabaseContext>();
+        
+        return services;
+    }
+}
---
+++ Database/Repositories/OrderRepository.cs
@@ -0,0 +1,44 @@
+﻿using Database.Context;
+using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Repositories;
+
+public class OrderRepository : BaseRepository<Order>
+{
+    private readonly DatabaseContext _databaseContext;
+
+    public OrderRepository(DatabaseContext databaseContext) : base(databaseContext)
+    {
+        _databaseContext = databaseContext;
+    }
+
+    public Task<List<Order>> GetOrdersByCustomerIdAsync(int customerId, bool includeDeleted = false)
+    {
+        return GetRecords(includeDeleted)
+            .Where(o => o.CustomerId == customerId)
+            .ToListAsync();
+    }
+
+    public Task<List<Order>> GetOrdersByDateRangeAsync(DateTime startDate, DateTime endDate,
+        bool includeDeleted = false)
+    {
+        return GetRecords(includeDeleted)
+            .Where(o => o.OrderDate >= startDate && o.OrderDate <= endDate)
+            .ToListAsync();
+    }
+
+    public Task<List<Order>> GetOrdersWithCustomerAsync(bool includeDeleted = false)
+    {
+        return GetRecords(includeDeleted)
+            .Include(o => o.Customer)
+            .ToListAsync();
+    }
+
+    public Task<Order?> GetOrderWithCustomerByIdAsync(int orderId, bool includeDeleted = false)
+    {
+        return GetRecords(includeDeleted)
+            .Include(o => o.Customer)
+            .FirstOrDefaultAsync(o => o.Id == orderId);
+    }
+}
