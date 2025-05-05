92f43c0c25d61cd8bdf4848decda291884b637b3
eduard214 <octavianpantazi@yahoo.com>
2025-05-05 15:52:55 +0300

    Add Customer and Order entity classes for database model

---
+++ Database/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Database.Entities;
+
+public class Customer : BaseEntity
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ Database/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Database.Entities;
+
+public class Order : BaseEntity
+{
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public virtual Customer Customer { get; set; }
+}
